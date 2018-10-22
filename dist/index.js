b5d00e9084301eda9ec522eb3db6ff6e45e4d3c0
Matt Fender <mhfender@gmail.com>
2018-10-22 13:18:18 -0600

    Transpile with babel to dist

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,9 @@
-export default function(search, url = window.location.href) {
-  const name = search.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-  const results = regex.exec(url);
-
+export default function (search) {
+  var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
+  var name = search.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  var results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
-
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
