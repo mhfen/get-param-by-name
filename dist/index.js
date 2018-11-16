c91cdc8a761ae1b2db8eef7fd653698556cbe027
Matt Fender <mhfender@gmail.com>
2018-11-16 16:25:50 -0700

    Add module handler to index

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
-export default function (search) {
+"use strict";
+
+var getParamByName = function getParamByName(search) {
   var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
   var name = search.replace(/[\[\]]/g, '\\$&');
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
@@ -6,4 +8,12 @@ export default function (search) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = getParamByName;
+} else if (typeof define === 'function' && define.amd) {
+  define('getParamByName', [], function (require, exports, module) {
+    return getParamByName;
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default function(search, url = window.location.href) {
+const getParamByName = function(search, url = window.location.href) {
   const name = search.replace(/[\[\]]/g, '\\$&');
   const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
   const results = regex.exec(url);
@@ -7,4 +7,12 @@ export default function(search, url = window.location.href) {
   if (!results[2]) return '';
 
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = getParamByName;
+} else if (typeof define === 'function' && define.amd) {
+  define('getParamByName', [], function(require, exports, module) {
+    return getParamByName;
+  });
 }
