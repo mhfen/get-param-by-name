93097be19dfa068f5932babc67efad94cdb83de5
Matt Fender <mhfender@gmail.com>
2018-11-17 10:53:35 -0700

    Revert to Babel 6 for jest dependency

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
-"use strict";
+'use strict';
 
 var getParamByName = function getParamByName(search) {
   var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
+
   var name = search.replace(/[\[\]]/g, '\\$&');
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
   var results = regex.exec(url);
+
   if (!results) return null;
   if (!results[2]) return '';
+
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 };
 
